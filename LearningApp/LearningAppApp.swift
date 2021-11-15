4b603b4c06618226b492ea0cb3a79ef5d595eba5
McKenzie Macdonald <kenziemacd@icloud.com>
2021-11-15 11:39:53 -0800

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by McKenzie Macdonald on 11/15/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
