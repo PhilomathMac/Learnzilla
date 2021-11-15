c24c29bb6506ca461a730cf3e188f960ea797e27
McKenzie Macdonald <kenziemacd@icloud.com>
2021-11-15 13:30:34 -0800

    - Create HomeViewRow view - Create HomeView layout

--- LearningApp/Views/HomeView.swift
+++ LearningApp/Views/HomeView.swift
@@ -10,16 +10,40 @@ import SwiftUI
 struct HomeView: View {
     //MARK: Properties
     @EnvironmentObject var model : ContentModel
+    
     //MARK: UI
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        
+        NavigationView {
+            VStack(alignment: .leading) {
+                Text("What would you like to do today?")
+                    .padding(.leading, 20)
+                ScrollView {
+                    LazyVStack {
+                        ForEach (model.modules) { module in
+                            
+                            VStack (spacing: 20){
+                                // Learning Card
+                                HomeViewRow(image: module.content.image, title: "Learn \(module.category)", description: module.content.description, countString: "\(module.content.lessons.count) Lessons", time: module.content.time)
+                                    
+                                // Test Card
+                                HomeViewRow(image: module.test.image, title: "\(module.category) Test", description: module.test.description, countString: "\(module.test.questions.count) Questions", time: module.test.time)
+                            }
+                        }
+                    }
+                    .padding()
+                }
+            }
+            .navigationTitle("Get Started")
+        }
     }
-    //MARK: Methods    
+    //MARK: Methods
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+            .environmentObject(ContentModel())
     }
 }
---
+++ LearningApp/Views/HomeViewRow.swift
@@ -0,0 +1,67 @@
+//
+//  HomeViewRow.swift
+//  LearningApp
+//
+//  Created by McKenzie Macdonald on 11/15/21.
+//
+
+import SwiftUI
+
+struct HomeViewRow: View {
+    
+    var image: String
+    var title: String
+    var description: String
+    var countString: String
+    var time: String
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .foregroundColor(.white)
+                .cornerRadius(10)
+                .shadow(radius: 5)
+                // Hard coded size - not good for adjustable sizes
+                //.frame(width: 335, height: 175)
+                // Will take up as much space as it can while maintaining aspect ratio
+                .aspectRatio(CGSize(width: 335, height: 175), contentMode: .fit)
+            
+            HStack {
+                Image(image)
+                    .resizable()
+                    .frame(width: 116, height: 116)
+                    .clipShape(Circle())
+                Spacer()
+                VStack(alignment: .leading, spacing: 10) {
+                    Text(title)
+                        .bold()
+                    Text(description)
+                        .font(.caption)
+                        .padding(.bottom, 20)
+                    HStack{
+                        Image(systemName: "text.book.closed")
+                            .resizable()
+                            .frame(width: 15, height: 15)
+                        Text(countString)
+                            .font(Font.system(size: 10))
+                        Spacer()
+                        Image(systemName: "clock")
+                            .resizable()
+                            .frame(width: 15, height: 15)
+                        Text(time)
+                            .font(Font.system(size: 10))
+                    }
+                }
+                .padding(.leading, 20)
+            }
+            .padding(.horizontal, 20)
+        }
+    }
+}
+
+struct HomeViewRow_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeViewRow(image: "swift", title: "Learn Swift", description: "testing testing testing description", countString: "10 Lessons", time: "2 Hours")
+    
+    }
+}
